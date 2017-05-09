8d8cdfef88e69db68c2cd1e12d868db08c530bf8
zaid kamil <xaidmetamorphos@gmail.com>
2017-05-10 03:48:47 +0530

    completed PodDroid

--- app/src/main/java/com/alfco/myrsspodcast/fragments/FragmentHome.java
+++ app/src/main/java/com/alfco/myrsspodcast/fragments/FragmentHome.java
@@ -182,7 +182,7 @@ public class FragmentHome extends BaseFragment implements RequestListener<byte[]
     private void initLogicalComponenets(){
         pod_pref = getActivity().getSharedPreferences("pod_pref", Context.MODE_PRIVATE);
         selected_url = pod_pref.getString("selected_url", "https://kindafamouspod.podbean.com/feed/");
-        etUrl.setText(Constants.url);
+        etUrl.setText(selected_url);
         player=new MediaPlayer();
     }
 
